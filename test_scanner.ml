95205edf9d02a78cd09dea2db9fc6c9b61c88836
Jayg000e <jg4692@columbia.edu>
2024-05-08 06:12:23 -0700

    remove redundant script

--- test_scanner.ml
+++
@@ -1,8 +0,0 @@
-let rec read_all_tokens lexbuf =
-  match Scanner.token lexbuf with
-  | Parse.EOF -> () (* End of input, stop the recursion *)
-  | _ -> read_all_tokens lexbuf (* Read the next token *)
-
-let _ =
-  let lexbuf = Lexing.from_channel stdin in
-  read_all_tokens lexbuf
